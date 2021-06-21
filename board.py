002ee7df87477ca5dfd4e24b4e538eaa39a95492
MarkGhebrial <mghebrialjr@icloud.com>
2021-06-20 17:02:45 -0700

    Code clenaup

--- board.py
+++ board.py
@@ -2,7 +2,6 @@ from street import Street
 from communityChest import CommunityChest
 from chance import Chance
 from goToJail import GoToJail
-#from player import Player
 from streetNames import StreetNames
 
 #------------------Create the list of streets---------------#
@@ -24,31 +23,3 @@ for i in [StreetNames.CHANCE_0, StreetNames.CHANCE_1, StreetNames.CHANCE_2]:
     streets[i.value] = Chance(i)
         
 #-----------------------------------------------------------#
-
-class Board:
-    def __init__ (self):
-#        self.player = Player()
-
-        '''#------------------Create the list of streets---------------#
-
-        # Fill the list with normal streets
-        self.streets = []
-        for s in StreetNames:
-            self.streets.append(Street(s))
-
-        # Override go to jail
-        self.streets[StreetNames.GO_TO_JAIL.value] = GoToJail()
-
-        # Override the community chests
-        for i in [StreetNames.COMMUNITY_CHEST_0, StreetNames.COMMUNITY_CHEST_1, StreetNames.COMMUNITY_CHEST_2]:
-            self.streets[i.value] = CommunityChest(i)
-
-        # Override the chances
-        for i in [StreetNames.CHANCE_0, StreetNames.CHANCE_1, StreetNames.CHANCE_2]:
-            self.streets[i.value] = Chance(i)
-        
-        #-----------------------------------------------------------#'''
-
-if __name__ == "__main__":
-    b = Board()
-    print(b.streets)
--- player.py
+++ player.py
@@ -1,5 +1,4 @@
 import random
-from streetNames import StreetNames
 from board import streets
 
 class Player:
@@ -8,9 +7,10 @@ class Player:
 
     def rollDice (self):
         self.currentPosition = (self.currentPosition + random.randint(0, 6) + random.randint(0, 6)) % 40 # Simulate rolling the dice
-        streets[self.currentPosition].incrementVisits()
-        lastPosition = self.currentPosition
+        streets[self.currentPosition].incrementVisits() # Increment the visit count of landed street
+        lastPosition = self.currentPosition 
 
         self.currentPosition = streets[self.currentPosition].getPlayerPosition()
+        # If the street caused the plaer to move, then increment the counter of the new street
         if lastPosition != self.currentPosition:
             streets[self.currentPosition].incrementVisits()
