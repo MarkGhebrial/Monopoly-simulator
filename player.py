4a1f0b2938cd476ea268b0e212ab2e6bed7911d7
MarkGhebrial <mghebrialjr@icloud.com>
2021-06-27 14:42:04 -0700

    Go to jail if too many doubles

--- player.py
+++ player.py
@@ -1,12 +1,30 @@
 import random
+from streetNames import StreetNames
 from board import streets
 
 class Player:
     def __init__ (self, startingPosition=0):
         self.currentPosition = startingPosition
+        self.doubleRolls = 0
 
     def rollDice (self):
-        self.currentPosition = (self.currentPosition + random.randint(0, 6) + random.randint(0, 6)) % 40 # Simulate rolling the dice
+        # Roll the dice
+        die1 = random.randint(1,6)
+        die2 = random.randint(1,6)
+
+        if die1 == die2:
+            self.doubleRolls += 1
+        else:
+            self.doubleRolls = 0
+
+        if self.doubleRolls == 3:
+            self.currentPosition = StreetNames.JAIL.value
+            streets[StreetNames.JAIL.value].incrementVisits()
+            self.doubleRolls = 0
+            print("rolled too many doubles")
+            return
+
+        self.currentPosition = (self.currentPosition + die1 + die2) % 40 # Advance the player
         streets[self.currentPosition].incrementVisits() # Increment the visit count of landed street
         lastPosition = self.currentPosition 
 
@@ -14,3 +32,12 @@ class Player:
         # If the street caused the plaer to move, then increment the counter of the new street
         if lastPosition != self.currentPosition:
             streets[self.currentPosition].incrementVisits()
+
+if __name__ == "__main__":
+    p = Player(StreetNames.JAIL.value + 1)
+    while p.currentPosition != StreetNames.JAIL.value:
+        p.currentPosition = 0
+        p.rollDice()
+        p.rollDice()
+        p.rollDice()
+        print(p.currentPosition)
